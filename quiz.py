2870177ca7c28e0494e21bc6888529a43eac458f
codysseyrookie <310622292+codysseyrookie@users.noreply.github.com>
2026-08-03 21:22:48 +0900

    Feat: Quiz 클래스 구현

---
+++ quiz.py
@@ -0,0 +1,21 @@
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def display(self):
+        print(self.question)
+
+        for index, choice in enumerate(self.choices, start=1):
+            print(f"{index}. {choice}")
+
+    def check_answer(self, user_answer):
+        return user_answer == self.answer
+
+    def to_dict(self):
+        return {
+            "question": self.question,
+            "choices": self.choices,
+            "answer": self.answer,
+        }
